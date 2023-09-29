c343c969b798e04e738eaa0606e5263a445f7093
CAROLKOECH <koechc44@gmail.com>
2023-09-30 00:01:58 +0300

    done

--- server/app.py
+++ server/app.py
@@ -2,8 +2,7 @@
 
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
+from flask_restful import Api, Resource, reqparse
 from models import db, Plant
 
 app = Flask(__name__)
@@ -13,15 +12,36 @@ app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
-
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return [p.to_dict() for p in plants], 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('image', type=str, required=True)
+        parser.add_argument('price', type=float, required=True)
+        args = parser.parse_args()
+
+        new_plant = Plant(name=args['name'], image=args['image'], price=args['price'])
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return new_plant.to_dict(), 201
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if plant:
+            return plant.to_dict(), 200
+        else:
+            return {"message": "Plant not found"}, 404
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
